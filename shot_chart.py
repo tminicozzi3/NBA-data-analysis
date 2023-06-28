d6dd3dd9eb0ad3948691547e20dd30c0f5c0315b
Timothy Minicozzi <timothyminicozzi@Timothys-MBP.fios-router.home>
2023-06-28 16:31:30 -0400

    succesfully load json

--- shot_chart.py
+++ shot_chart.py
@@ -27,3 +27,11 @@ def get_player_id(player_name):
 jp_id = get_player_id("jordan poole")
 print(jp_id)
 
+jp_data = shotchartdetail.ShotChartDetail(
+    team_id=0,
+    player_id=jp_id,
+    season_nullable='2022-23',
+    season_type_all_star='Regular Season'
+)
+
+print(json.loads(jp_data.get_json()))
