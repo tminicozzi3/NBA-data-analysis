6a1126c3bacca32f252d5aff6fd1efa5b96be137
Timothy Minicozzi <timothyminicozzi@Timothys-MBP.fios-router.home>
2023-06-28 16:46:43 -0400

    add function

--- shot_chart.py
+++ shot_chart.py
@@ -14,15 +14,36 @@ import matplotlib.pyplot as plt
 
 def get_player_id(player_name):
     """
-    input player name: str, not case sensitive
-    output: player_id if valid player and only one player by that name
-            -1 if not a valid player or there are more than one player with the name
+    inputs:
+        player name: str, not case sensitive
+    output:
+        player_id if valid player and only one player by that name
+        -1 if not a valid player or there are more than one player with the name
     """
     list_player = players.find_players_by_full_name(player_name)
     if len(list_player) == 1:
         return list_player[0]["id"]
     else:
         return -1
+
+def get_json(player_id, year, regular_or_post_season, team_id = 0):
+    """
+    inputs:
+        team_id: int, default is 0 (looks at all shots player takes no matter team they are on)
+        player_id: int, id number of player in data
+        year: str, of the form yyyy-yy
+        regular_or_post_season: str, either "Regular Season", "Playoffs", "Pre Season"
+            or "All Star"
+    output:
+        json file containing requested data
+    """
+    data = shotchartdetail.ShotChartDetail(
+        team_id=team_id,
+        player_id=player_id,
+        season_nullable=year,
+        season_type_all_star=regular_or_post_season
+    )
+    return json.loads(data.get_json())
 
 jp_id = get_player_id("jordan poole")
 print(jp_id)
@@ -34,4 +55,6 @@ jp_data = shotchartdetail.ShotChartDetail(
     season_type_all_star='Regular Season'
 )
 
-print(json.loads(jp_data.get_json()))
+# print(json.loads(jp_data.get_json()))
+
+print(get_json(jp_id, "2022-23", "Regular Season"))
