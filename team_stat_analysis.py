dade59f9b32b2be033b17a0c3188fe6ba61a6910
Timothy Minicozzi <timothyminicozzi@Timothys-MBP.hsd1.ma.comcast.net>
2023-08-15 14:58:35 -0400

    further ideas

--- team_stat_analysis.py
+++ team_stat_analysis.py
@@ -6,13 +6,42 @@ import requests
 import json
 import pandas as pd
 
-def generate_df(url, headers):
+def get_url(team_id, year):
+    """
+    inputs:
+        team_id: str, id of team, "0" if want all teams
+        year: str, year you want to observe stats for
+    output:
+        url: the url for the data you want
+    """
+    url = "https://stats.nba.com/stats/leaguedashteamstats?Conference=&DateFrom=&DateTo="+\
+    "&Division=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location="+\
+    "&MeasureType=Scoring&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N"+\
+    "&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N"+\
+    "&Season="+year+"&SeasonSegment=&SeasonType=Playoffs&ShotClockRange=&StarterBench="+\
+    "&TeamID="+team_id+"&TwoWay=0&VsConference=&VsDivision="
+    return url
+    return "https://stats.nba.com/stats/leaguedashteamstats?Conference=&DateFrom=&DateTo=&Division=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Scoring&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2022-23&SeasonSegment=&SeasonType=Playoffs&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision="
+
+def generate_df(url):
     """
     input:
         url: str, url for which we want to generate a dataframe
     output:
         df: dataframe with data from url
     """
+    headers = {"Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Host": "stats.nba.com",
+    "Origin": "https://www.nba.com",
+    "Referer": "https://www.nba.com/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15"}
+    
     # create dict
     data = requests.get(url, headers = headers).json()
     # data = json.loads(response.text)
@@ -25,23 +54,16 @@ def generate_df(url, headers):
     return df
 
 if __name__ == '__main__':
-    # the NBA is hard to analyze because of the talent differential between teams and the
-    # lack of effort by players during the regular season
+    # the NBA is hard to analyze because of the talent and style differential between teams
+    # and the lack of effort by players during the regular season
     # for our analysis, we will focus on the Milwaukee Bucks playoff performance over a
     # period of years where their core roster has remained largely intact
     # hopefully, this provides some insight on the key drivers of what wins games
     # measure relationship between stat and playoff winning percentage
-    url = "https://stats.nba.com/stats/leaguedashteamptshot?CloseDefDistRange=&College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&DribbleRange=&GameScope=&GameSegment=&GeneralRange=&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2022-23&SeasonSegment=&SeasonType=Regular%20Season&ShotClockRange=7-4%20Late&ShotDistRange=&StarterBench=&TeamID=0&TouchTimeRange=&VsConference=&VsDivision=&Weight="
-    headers = {"Accept": "*/*",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Connection": "keep-alive",
-    "Host": "stats.nba.com",
-    "Origin": "https://www.nba.com",
-    "Referer": "https://www.nba.com/",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-site",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15"}
-    d = generate_df(url, headers)
+    milwaukee_id = "1610612749"
+    years = ["2017-18", "2018-19", "2019-20", "2020-21", "2021-22", "2022-23"]
+    url = get_url(milwaukee_id, years[-1])
+    d = generate_df(url)
+    d = d.drop(columns=[c for c in d.columns if "RANK" in c])
     print(d)
+    print(d.columns)
