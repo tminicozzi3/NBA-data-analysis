0f4afa0e49d0c6a0bdfccadf83388faeb3441870
Timothy Minicozzi <timothyminicozzi@Timothys-MBP.hsd1.ma.comcast.net>
2023-08-16 10:30:18 -0400

    perform regression analysis

--- team_stat_analysis.py
+++ team_stat_analysis.py
@@ -5,6 +5,8 @@
 import requests
 import json
 import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 def get_url(team_id, year):
     """
@@ -14,6 +16,8 @@ def get_url(team_id, year):
     output:
         url: the url for the data you want
     """
+    # this url is for the stats page that shows percentages of total scoring that come
+    # from various shot types
     url = "https://stats.nba.com/stats/leaguedashteamstats?Conference=&DateFrom=&DateTo="+\
     "&Division=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location="+\
     "&MeasureType=Scoring&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N"+\
@@ -21,15 +25,16 @@ def get_url(team_id, year):
     "&Season="+year+"&SeasonSegment=&SeasonType=Playoffs&ShotClockRange=&StarterBench="+\
     "&TeamID="+team_id+"&TwoWay=0&VsConference=&VsDivision="
     return url
-    return "https://stats.nba.com/stats/leaguedashteamstats?Conference=&DateFrom=&DateTo=&Division=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Scoring&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2022-23&SeasonSegment=&SeasonType=Playoffs&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision="
 
-def generate_df(url):
+def generate_df(url, year):
     """
     input:
         url: str, url for which we want to generate a dataframe
+        year: str, year YYYY-YY
     output:
         df: dataframe with data from url
     """
+    # need to include headers to be able to access data
     headers = {"Accept": "*/*",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-US,en;q=0.9",
@@ -41,17 +46,53 @@ def generate_df(url):
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "same-site",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15"}
-    
     # create dict
     data = requests.get(url, headers = headers).json()
-    # data = json.loads(response.text)
-
     # create df from dict
     data_headers = data["resultSets"][0]["headers"]
     data_set = data["resultSets"][0]["rowSet"]
     df = pd.DataFrame(data_set, columns = data_headers)
+    # add a year column to data
+    df["YEAR"] = year
 
     return df
+
+def generate_all_data_df(urls, years):
+    """
+    input:
+        urls: list of str, url for which we want to generate a dataframe
+        years: list of str of form YYYY-YY
+    output:
+        df: dataframe with data from url over all years
+    """
+    # loop through the data corresponding to different years
+    df = generate_df(urls[0], years[0])
+    for url, year in zip(urls[1:],years[1:]):
+        # create the df for the data
+        new_df = generate_df(url, year)
+        # add that df to the all years df
+        df = pd.concat([df, new_df], axis = 0)
+    return df
+
+def calc_r2(df):
+    """
+    input:
+        df: dataframe with data of interest
+    output:
+        lst_r2: list of tuple...(stat, r2 value of stat wrt win pct)
+    """
+    # stats of interest
+    metrics = [c for c in df.columns if ("PCT" in c and c != "W_PCT")]
+    lst_r2 = []
+    # loop through stats
+    for metric in metrics:
+        # create a linear regression model with appropriate data
+        model = LinearRegression()
+        X, y = np.array(df[metric]).reshape(-1, 1), np.array(df["W_PCT"])
+        model.fit(X, y)
+        # find r2 value and add to list
+        lst_r2.append((metric, round(model.score(X, y), 3)))
+    return lst_r2
 
 if __name__ == '__main__':
     # the NBA is hard to analyze because of the talent and style differential between teams
@@ -62,8 +103,10 @@ if __name__ == '__main__':
     # measure relationship between stat and playoff winning percentage
     milwaukee_id = "1610612749"
     years = ["2017-18", "2018-19", "2019-20", "2020-21", "2021-22", "2022-23"]
-    url = get_url(milwaukee_id, years[-1])
-    d = generate_df(url)
+    urls = [get_url(milwaukee_id, year) for year in years]
+    d = generate_all_data_df(urls, years)
     d = d.drop(columns=[c for c in d.columns if "RANK" in c])
-    print(d)
-    print(d.columns)
+    # print(d)
+    # print(d.columns)
+    # print(list(d["W_PCT"]))
+    print(calc_r2(d))
